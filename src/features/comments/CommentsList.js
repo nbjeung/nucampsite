ca976cb4b83968625f8709c5ba946469911cac0a
Nicolas J <njeung@frontiernet.net>
2023-10-28 13:02:23 -0700

    Comment Form Validation

--- src/features/comments/CommentsList.js
+++ src/features/comments/CommentsList.js
@@ -1,6 +1,8 @@
-import { Col } from "reactstrap";
+import { Col } from 'reactstrap';
 import Comment from './Comment';
-import { selectCommentsByCampsiteId } from "./commentsSlice";
+import { selectCommentsByCampsiteId } from './commentsSlice';
+import CommentForm from './CommentForm';
+// import AnimatedComment from './AnimatedComment';
 
 const CommentsList = ({ campsiteId }) => {
     const comments = selectCommentsByCampsiteId(campsiteId);
@@ -9,9 +11,17 @@ const CommentsList = ({ campsiteId }) => {
         return (
             <Col md='5' className='m-1'>
                 <h4>Comments</h4>
-                {comments.map((comment) => {
-                    return <Comment key={comment.id} comment={comment} />;
+                {comments.map((comment, idx) => {
+                    return (
+                        <Comment key={comment.id} comment={comment} />
+                        // <AnimatedComment
+                        //     key={comment.id} 
+                        //     comment={comment}
+                        //     idx={idx}
+                        // />
+                    );
                 })}
+                <CommentForm campsiteId={campsiteId}></CommentForm>
             </Col>
         );
     }
